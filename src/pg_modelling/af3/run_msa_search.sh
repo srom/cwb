70df1e782d50d70cfb5ced478bca8a265e88afac
Romain Strock <romain.strock@gmail.com>
2025-02-10 23:47:51 +0000

    One more try

--- src/pg_modelling/af3/run_msa_search.sh
+++ src/pg_modelling/af3/run_msa_search.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=af3_msa_run
 #SBATCH --output={log_path}
-#SBATCH --partition=gpu
+#SBATCH --partition=cpu
 #SBATCH --qos=qos_batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1
 #SBATCH --mem=32G
 #SBATCH --time={time_budget}
 
 set -e
 
-export JAX_PLATFORM_NAME=cpu
-export CUDA_VISIBLE_DEVICES=""
-
 INPUT="{input}"
 INPUT="{output}"
 
-module load alphafold/3.0.1
+module load alphafold/3.0.0
 
 alphafold \
   --input_dir $INPUT \

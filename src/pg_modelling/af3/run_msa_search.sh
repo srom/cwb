d95a3afc08365f7a21c55860f0250614074814fa
Romain Strock <romain.strock@gmail.com>
2025-02-11 00:03:13 +0000

    Run MSA step on GPU node for af3

--- src/pg_modelling/af3/run_msa_search.sh
+++ src/pg_modelling/af3/run_msa_search.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=af3_msa_run
 #SBATCH --output={log_path}
-#SBATCH --partition=cpu
+#SBATCH --partition=gpy
 #SBATCH --qos=qos_batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
 #SBATCH --mem=32G
 #SBATCH --time={time_budget}
 
